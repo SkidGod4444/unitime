3abf189d010437a83714d398346bbf20ab33d1cb
Saidev Dhal (Dev) <connect.saidev@gmail.com>
2026-02-02 23:10:52 +0530

    fix

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,4 +1,19 @@
+import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
-import app from './v1'
+import attendance from '../src/routes/attendance'
+import courses from '../src/routes/courses'
+import history from '../src/routes/history'
+import notifications from '../src/routes/notifications'
+import timetable from '../src/routes/timetable'
+import users from '../src/routes/users'
+
+const app = new Hono().basePath('/v1')
+
+app.route('/users', users)
+app.route('/attendance', attendance)
+app.route('/history', history)
+app.route('/notifications', notifications)
+app.route('/timetable', timetable)
+app.route('/courses', courses)
 
 export default handle(app)
